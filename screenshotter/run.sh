524f1ca896301ef5cb79ec5b081533a493d67c88
Christophe De Troyer <christophe.detroyer@gmail.com>
2022-05-01 17:54:07 +0200

    remove old

--- screenshotter/run.sh
+++ screenshotter/run.sh
@@ -5,5 +5,7 @@ website=$1
 docker run -i --rm -v $(pwd):/data m1dnight/screenshotter $1
 
 # Send to webspace.
+rm -f ${2}/yellow.dat
 mv yellow.dat ${2}/yellow.dat
+rm -f ${2}/image.dat
 mv image.dat ${2}/image.dat
