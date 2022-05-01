5fb866f69897a99097caa73b50c71567285b0e38
Christophe De Troyer <christophe.detroyer@gmail.com>
2022-05-01 17:51:06 +0200

    move to local files

--- screenshotter/run.sh
+++ screenshotter/run.sh
@@ -5,8 +5,5 @@ website=$1
 docker run -i --rm -v $(pwd):/data m1dnight/screenshotter $1
 
 # Send to webspace.
-scp yellow.dat christophe@earth.local:/home/christophe/public_html/yellow.dat 
-scp image.dat christophe@earth.local:/home/christophe/public_html/image.dat 
-
-# Cleanup 
-rm -f yellow.dat image.dat 
+mv yellow.dat ${2}/yellow.dat
+mv image.dat ${2}/image.dat
